eb43c121aa3a93ee272c3f38eff90dd71ab885c6
John <124292133+CheYoki@users.noreply.github.com>
2025-04-19 19:54:11 -0300

    Update _MSC_VER max version to 1944
    Updated _MSC_VER upper bound to 1944 (VS 2022 17.14). The project builds successfully in more recent Visual Studio environments.

--- third_party/ruby_extern/config/x86/ruby/config.h
+++ third_party/ruby_extern/config/x86/ruby/config.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_RUBY_CONFIG_H
 #define INCLUDE_RUBY_CONFIG_H 1
-#if (_MSC_VER < 1920) || (_MSC_VER > 1939)
-#error MSC version unmatch: 1920..1939 is expected.
+#if (_MSC_VER < 1920) || (_MSC_VER > 1944)
+#error MSC version unmatch: 1920..1944 is expected.
 #endif
 #define RUBY_MSVCRT_VERSION 140
 #define STDC_HEADERS 1
