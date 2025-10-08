73896ba6bf493ad681326648cb7b074585c4bd30
Admenri Adev <admenri0504@gmail.com>
2025-10-08 13:30:39 +0800

    Added storage clear for emscripten.

--- binding/mri/platform/mri_emscripten.cc
+++ binding/mri/platform/mri_emscripten.cc
@@ -15,7 +15,7 @@ EM_JS(void, emjs_storage_save_file, (const char* filePath), {
     const fileData = FS.readFile(u8Path);
 
     const catchBlock = (err) => {
-      console.log('urge.save_file error', err);
+      console.log('urge.storage_save_file error', err);
     };
 
     window.localforage.setItem(u8Path, fileData).then(wakeUp).catch(catchBlock);
@@ -33,7 +33,7 @@ EM_JS(void, emjs_storage_load_file, (const char* filePath), {
     };
 
     const catchBlock = (err) => {
-      console.log('urge.load_file error', err);
+      console.log('urge.storage_load_file error', err);
     };
 
     window.localforage.getItem(u8Path).then(fileLoader).catch(catchBlock);
@@ -49,10 +49,30 @@ EM_JS(void, emjs_storage_load_all_files, (void), {
     };
 
     const catchBlock = (err) => {
-      console.log('urge.load_all_files error', err);
+      console.log('urge.storage_load_all_files error', err);
     };
 
     window.localforage.iterate(iterFile).then(wakeUp).catch(catchBlock);
+  };
+
+  Asyncify.handleSleep(asyncLoader);
+});
+
+EM_JS(void, emjs_storage_clear, (const char* filePath), {
+  const asyncLoader = (wakeUp) => {
+    const u8Path = UTF8ToString(filePath);
+
+    const catchBlock = (err) => {
+      console.log('urge.storage_clear error', err);
+    };
+
+    if (u8Path == '') {
+      // Clear all
+      window.localforage.clear().then(wakeUp).catch(catchBlock);
+    } else {
+      // Clear specific target
+      window.localforage.removeItem(u8Path).then(wakeUp).catch(catchBlock);
+    }
   };
 
   Asyncify.handleSleep(asyncLoader);
@@ -100,6 +120,15 @@ MRI_METHOD(em_storage_load_all_files) {
   return Qnil;
 }
 
+MRI_METHOD(em_storage_clear) {
+  MriCheckArgc(argc, 1);
+
+  auto* path_str = RSTRING_PTR(argv[0]);
+  emjs_storage_clear(path_str);
+
+  return Qnil;
+}
+
 MRI_METHOD(em_preload_file) {
   MriCheckArgc(argc, 1);
 
@@ -125,6 +154,7 @@ void InitEmscriptenBinding() {
   MriDefineModuleFunction(module, "storage_load_file", em_storage_load_file);
   MriDefineModuleFunction(module, "storage_load_all_files",
                           em_storage_load_all_files);
+  MriDefineModuleFunction(module, "storage_clear", em_storage_clear);
 
   MriDefineModuleFunction(module, "preload_file", em_preload_file);
   MriDefineModuleFunction(module, "is_preload_file_cached",
